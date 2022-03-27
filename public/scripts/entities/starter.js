e1a821bad4672409ec9e2e35a4927a6b943996c2
Kurt Maglalang <kbm126@live.com>
2022-03-27 05:52:36 +0000

    randomly change the background color of the fish tank every time the volcano is clicked

--- public/scripts/entities/starter.js
+++ public/scripts/entities/starter.js
@@ -2,12 +2,20 @@ class Starter extends Denizen {
 
   constructor(options) {
     super(options);
-    this.imageUri = '/images/volcano.jpg';
+    this.imageUri = '/images/Volcano-eruption-molten-lava-catastrophic-512.webp';
     this.position.y += this.height;
   }
 
   update(t) {
     // no physics for Starter
+  }
+
+  clickEffect() {
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    document.body.style.background = bgColor;
   }
 
   onClick(event) {
@@ -19,5 +27,6 @@ class Starter extends Denizen {
       velocity: new Vector(xVel, yVel),
       type: this.tank.getRandomSpecies(),
     });
+    this.clickEffect();
   }
 }
